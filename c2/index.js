3db0fe710fe07b57ff58d0cde9d128ef055de399
Aleksandar Milosheski <73908689+aleks-123@users.noreply.github.com>
2024-12-25 21:38:58 +0100

    c4

---
+++ c2/index.js
@@ -0,0 +1,21 @@
+// const kalkulator = require('./kalkulator.js');
+
+// const rezultatSobiranje = kalkulator.sobiranje(2, 5);
+// console.log(rezultatSobiranje);
+// const rezulatOdzemanje = kalkulator.odzemanje(10, 4);
+// console.log(rezulatOdzemanje);
+
+const { sobiranje, mnozenje } = require('./kalkulator.js');
+const rezulatSobiranje = sobiranje(3, 10);
+console.log(rezulatSobiranje);
+const rezulatMnozeznje = mnozenje(6, 6);
+console.log(rezulatMnozeznje);
+
+//todo: MODUL VAGA.JS
+//todo: KILOGRAM VO GRAM I OBRATNO
+//todo: LITAR VO MILITAR I OBRATNO
+
+//? domasna.js ime na moduloy
+//? FARENHAJT VO CELZIUS i obratno
+//? PLOSTINA I PERIMETAR NA TRIAGOLNIK I NA PRAVOAGOLNIK
+//? PROVERRUVA DALI NEKOJ BROJ E PAREN ILI NE
