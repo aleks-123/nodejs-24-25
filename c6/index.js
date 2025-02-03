31f30b6ee0950c367ecd5ab0b73c53666510a733
Aleksandar Milosheski <73908689+aleks-123@users.noreply.github.com>
2025-02-03 22:01:21 +0100

    c9

--- c6/index.js
+++ c6/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('home ruta');
 });
@@ -19,7 +21,6 @@ app.get('/calculator/:op/:a/:b', (req, res) => {
   const broj1 = Number(req.params.a);
   const broj2 = Number(req.params.b);
   const operacija = req.params.op;
-
   switch (req.params.op) {
     case 'sobiranje':
       return res.send(`${Number(req.params.a) + Number(req.params.b)}`);
@@ -34,9 +35,37 @@ app.get('/calculator/:op/:a/:b', (req, res) => {
   }
 });
 
+const kalkulatorFunkcija = (req, res) => {
+  switch (req.body.operacija) {
+    case 'sobiranje':
+      return res.send(`${Number(req.body.a) + Number(req.body.b)}`);
+    case 'odzemanje':
+      return res.send(`${Number(req.body.a) - Number(req.body.b)}`);
+    case 'delenje':
+      return res.send(`${Number(req.body.a) / Number(req.body.b)}`);
+    case 'mnozenje':
+      return res.send(`${Number(req.body.a) * Number(req.body.b)}`);
+    default:
+      return res.send('Nepoznat operator');
+  }
+};
+
+app.post('/calculator', kalkulatorFunkcija);
+
 // app.get('/ime/:name', (req,res) => {})
 
 app.listen(10000, (err) => {
   if (err) return console.log(err.message);
   console.log('Server started successfully on port 10000');
 });
+
+// https://www.npmjs.com/package/currency-converter-lt
+// https://github.com/paul-shuvo/nodejs-crypto-converter
+// https://www.coingecko.com/en/api
+
+//! MVC
+//! M - Model - VO modelot stoi logikata na podatocite (biznis logika)
+//! V - View - View e zagrizen za prezentacija na izgledot (Grafikata)
+//! C - Controller - Kontrolerot e mozokot na nashata stranica, koj vrshi interakcija so modelot i view
+
+//? 5 ruti so post metoda
